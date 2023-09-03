0d93b1e78908b7dc8e89539d59654154d7934fef
icchigoo <icchigoo12@gmail.com>
2023-09-03 19:06:29 +0545

    member working

--- lib/widgets/create_transaction.dart
+++ lib/widgets/create_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticketing_system/provider/memberProvider.dart';
 import 'package:ticketing_system/provider/transactionProvider.dart';
 import 'package:ticketing_system/provider/serviceProvider.dart'; // Import your ServiceProvider
 
@@ -20,12 +21,15 @@ class _CreateTransactionDialogState extends State<CreateTransactionDialog> {
   final TextEditingController _totalAmountController = TextEditingController();
   int? _selectedServiceId; // Store the selected service ID
   List<dynamic> _services = []; // List to store fetched services
+  int? _selectedMemberId; // Store the selected membership ID
+  List<dynamic> _members = []; // List to store fetched members
 
   @override
   void initState() {
     super.initState();
     // Fetch services when the widget is initialized
     _fetchServices();
+    _fetchMembers();
   }
 
   // Fetch services using the ServiceProvider
@@ -38,6 +42,18 @@ class _CreateTransactionDialogState extends State<CreateTransactionDialog> {
       });
     } catch (e) {
       print('Error fetching services: $e');
+    }
+  }
+
+  Future<void> _fetchMembers() async {
+    try {
+      final memberProvider = MemberProvider(); // Initialize MemberProvider
+      await memberProvider.fetchMembers();
+      setState(() {
+        _members = memberProvider.members;
+      });
+    } catch (e) {
+      print('Error fetching members: $e');
     }
   }
 
@@ -65,7 +81,7 @@ class _CreateTransactionDialogState extends State<CreateTransactionDialog> {
                   });
                 },
                 decoration: const InputDecoration(
-                  labelText: 'Service ID',
+                  labelText: 'Service Name',
                   border: OutlineInputBorder(),
                 ),
                 validator: (value) {
@@ -76,12 +92,29 @@ class _CreateTransactionDialogState extends State<CreateTransactionDialog> {
                 },
               ),
               const SizedBox(height: 16),
-              TextFormField(
-                controller: _membershipIdController,
+              DropdownButtonFormField<int>(
+                value: _selectedMemberId,
+                items: _members.map<DropdownMenuItem<int>>((member) {
+                  return DropdownMenuItem<int>(
+                    value: member['id'],
+                    child: Text(member['memberName']),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedMemberId = newValue;
+                  });
+                },
                 decoration: const InputDecoration(
-                  labelText: 'Membership ID',
+                  labelText: 'Membership Name',
                   border: OutlineInputBorder(),
                 ),
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please select a membership ID';
+                  }
+                  return null;
+                },
               ),
               const SizedBox(height: 16),
               TextFormField(
@@ -126,7 +159,9 @@ class _CreateTransactionDialogState extends State<CreateTransactionDialog> {
         ),
         ElevatedButton(
           onPressed: () async {
-            if (_formKey.currentState!.validate() && _selectedServiceId != null) {
+            if (_formKey.currentState!.validate() &&
+                _selectedServiceId != null &&
+                _selectedMemberId != null) {
               final membershipId = int.tryParse(_membershipIdController.text);
               final discountAmount =
                   double.tryParse(_discountAmountController.text) ?? 0.0;
@@ -136,7 +171,8 @@ class _CreateTransactionDialogState extends State<CreateTransactionDialog> {
 
               await TransactionProvider().createTransaction(
                 serviceId: _selectedServiceId!,
-                membershipId: membershipId,
+                membershipId:
+                    _selectedMemberId!, // Pass the selected membership ID
                 discountAmount: discountAmount,
                 discountPercent: discountPercent,
                 totalAmount: totalAmount,
